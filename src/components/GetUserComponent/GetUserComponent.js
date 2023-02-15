0716fe9bdb02bf15a55999e28dde0ef2c1794d17
Karol <karolrds01@gmail.com>
2023-02-15 18:59:09 -0300

    second commit

--- src/components/GetUserComponent/GetUserComponent.js
+++ src/components/GetUserComponent/GetUserComponent.js
@@ -3,19 +3,21 @@ import { useState } from "react";
 function GetUserComponent({setUser, setIsLoggedIn}){
     const [userInput, setUserInput] = useState('')
     const handleLogin = ()=>{
-        setUser(userInput)
+        setUser = userInput.value
         setIsLoggedIn(true)
     }
     return(
-        <form onSubmit={(event) => handleLogin(event.preventDefault())}>
-             <input
-                type="text"
-                placeholder="Insira seu nome"
-                value={userInput}
-                onChange={(event) => setUserInput(event.target.value)} 
-                />
-                <button type="submit">Acessar com o nome</button>
-        </form>
+        <div>
+            <form onSubmit={(event) => handleLogin(event.preventDefault())}>
+                <input
+                    type="text"
+                    placeholder="Insira seu nome"
+                    value={userInput}
+                    onChange={(event) => setUserInput(event.target.value)} 
+                    />
+                    <button type="submit">Acessar com o nome</button>
+            </form>
+        </div>
     )
 }
 export default GetUserComponent
